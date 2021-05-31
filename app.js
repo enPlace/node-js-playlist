0e34a09366f1c05629ce29cfb2cef193ceee30af
enPlace <n.place@protonmail.com>
2021-05-31 13:33:25 -0300

    timer

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ let time = 0;
 function counter(){
     time +=2
     console.log(time +" seconds have passed")
+    if(time>5){
+        clearInterval(timer)
+        console.log("out of time")
+    }
 }
-setInterval(counter, 2000)
+let timer = setInterval(counter, 2000)
